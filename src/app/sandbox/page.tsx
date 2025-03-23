daefb926de28ebb591db41529f593ab49c464049
korih <96402323+korih@users.noreply.github.com>
2025-03-22 18:13:24 -0700

    added mock data and way to add it to db

---
+++ src/app/sandbox/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { db } from "~/server/db"
+import { mockFolders, mockFiles } from '~/lib/mock-data'
+import { files, folders } from '~/server/db/schema';
+
+export default function SandboxPage() {
+  return (
+    <div className='flex flex-col gap-4'>
+      Seed Function
+      <form action={async () => {
+        "use server";
+
+        const folderInsert = await db.insert(folders).values(
+          mockFolders.map((folder, index) => ({
+            id: index + 1,
+            name: folder.name,
+            parent: index !== 0 ? 1 : 1,
+          })), );
+
+        const fileInsert = await db.insert(files).values(mockFiles.map((file, index) => ({
+          id: index + 1,
+          name: file.name,
+          size: 50000,
+          url: file.url,
+          parent: (index % 3) + 1,
+        })))
+
+        console.log(folderInsert)
+        console.log(fileInsert)
+
+      }}>
+        <button type="submit">CLICK ME</button>
+      </form>
+    </div>
+  )
+}
+
